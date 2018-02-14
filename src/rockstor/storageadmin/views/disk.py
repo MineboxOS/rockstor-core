3e97199f779071f996e25ff3533637f966656282
Robert Kaiser <kairo@kairo.at>
2018-02-14 20:49:48 +0100

    MIN-94: identify and import nbd devices and remove some unnecessary functionality for them

--- src/rockstor/storageadmin/views/disk.py
+++ src/rockstor/storageadmin/views/disk.py
@@ -48,7 +48,7 @@ logger = logging.getLogger(__name__)
 # and the post processing present in scan_disks()
 # LUKS currently stands for full disk crypto container.
 SCAN_DISKS_KNOWN_ROLES = ['mdraid', 'root', 'LUKS', 'openLUKS', 'bcache',
-                          'bcachecdev', 'partitions', 'LVM2member']
+                          'bcachecdev', 'nbd', 'partitions', 'LVM2member']
 WHOLE_DISK_FORMAT_ROLES = ['LUKS', 'bcache', 'bcachecdev', 'LVM2member']
 
 
@@ -269,6 +269,10 @@ class DiskMixin(object):
                 # Assigning a role to avoid these devices being seen as unused.
                 # The current value of this role is a placeholder and unused.
                 disk_roles_identified['LVM2member'] = str(d.fstype)
+            if d.name.startswith('nbd'):
+                # Network block device: we will use it like a one-partition
+                # disk but not report any advanced functionality.
+                disk_roles_identified['nbd'] = d.name
             if d.root is True:
                 # ROOT DISK: scan_disks() has already identified the current
                 # truth regarding the device hosting our root '/' fs so update
@@ -318,6 +322,20 @@ class DiskMixin(object):
                 # setup our special root disk db entry in Pool
                 # TODO: dynamically retrieve raid level.
                 p = Pool(name=d.label, raid='single', role='root')
+                p.save()
+                p.disk_set.add(dob)
+                # update disk db object to reflect special root pool status
+                dob.pool = p
+                dob.save()
+                p.size = p.usage_bound()
+                enable_quota(p)
+                p.uuid = btrfs_uuid(dob.name)
+                p.save()
+            # If no pool has yet been found with this disk's label in and
+            # the attached disk is our nbd disk
+            if (dob.pool is None and d.name.startswith('nbd') and d.label):
+                # setup a Pool dynamically
+                p = Pool(name=d.label, raid='single')
                 p.save()
                 p.disk_set.add(dob)
                 # update disk db object to reflect special root pool status
